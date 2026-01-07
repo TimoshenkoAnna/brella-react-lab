65207dc0da3c61a3b57392a08fbfb8552692dfc2
Тимошенко Анна <timoshenkoas.pir231@gmail.com>
2026-01-08 02:30:40 +0300

    feat: Add Container component for main layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
 import HomePage from './pages/HomePage.js';
@@ -9,16 +10,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <div className="app-wrapper bg-light">
       <Header />
-      <main className="main-content">
+      <Container as="main" className="my-4 flex-grow-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </main>
+      </Container>
       <Footer />
     </div>
   );
