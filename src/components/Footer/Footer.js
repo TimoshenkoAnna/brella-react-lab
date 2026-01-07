dc5b4ebe39adc6e6de1188e515e572246fca2a4d
Тимошенко Анна <timoshenkoas.pir231@gmail.com>
2026-01-08 02:15:39 +0300

    feat: Refactor Header with Navbar and Nav components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,30 @@
-import './Footer.css';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-function Footer() {
+function Header() {
   return (
-    <footer className="footer">
-        <div className="footer-container">
-            <p>© 2025 Custom Clothes. Все права защищены.</p>
-        </div>
-    </footer>
+    <Navbar bg="light" expand="lg" sticky="top" collapseOnSelect className="border-bottom">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand className="fw-bold">Custom Clothes</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Главная</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalog">
+              <Nav.Link>Каталог</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>О нас</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-export default Footer;
+
+export default Header;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,30 @@
-import { Link } from 'react-router-dom'; 
-import './Header.css';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function Header() {
   return (
-    <header className="header">
-        <div className="header-container">
-          {}
-          <Link to="/" className="header-logo">Custom Clothes</Link>
-          <nav className="header-nav">
-            <Link to="/">Главная</Link>
-            <Link to="/catalog">Каталог</Link>
-            <Link to="/about">О нас</Link>
-          </nav>
-        </div>
-    </header>
+    <Navbar bg="light" expand="lg" sticky="top" collapseOnSelect className="border-bottom">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand className="fw-bold">Custom Clothes</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Главная</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalog">
+              <Nav.Link>Каталог</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>О нас</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
 export default Header;
