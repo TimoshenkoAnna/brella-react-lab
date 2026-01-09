6cca61707b7842c1e8b5c837ea9caaf2c8b4c633
Тимошенко Анна <timoshenkoas.pir231@gmail.com>
2026-01-10 02:08:12 +0300

    refactor: Migrate CatalogPage state management to Redux

--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Row, Col, Button, Modal, Form, Image, Dropdown, DropdownButton, OverlayTrigger, Tooltip } from 'react-bootstrap';
@@ -49,9 +48,6 @@ const ProductForm = ({ product, onSave }) => {
 };
 
 function CatalogPage() {
-
-  const { t } = useTranslation();
-
   const dispatch = useDispatch();
 
   const products = useSelector(selectAllProducts);
@@ -92,7 +88,7 @@ function CatalogPage() {
     <div>
       <Row className="mb-4 align-items-center gy-3">
         <Col md={4}>
-           <h2 className="mb-0">{t('catalog_page.title')}</h2>
+            <h2 className="mb-0">Каталог</h2>
             <p className="text-muted mb-0">Найдено: {processedProducts.length} | Выбрано: {selectedIds.length}</p>
         </Col>
         <Col md={5}>
