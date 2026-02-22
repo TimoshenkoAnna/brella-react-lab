0c02316e1028a7d7a3d2ba75212054aa834496b0
Тимошенко Анна <timoshenkoas.pir231@gmail.com>
2026-02-22 15:20:43 +0300

    init: Start Lab 6, setup Express server and install dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// src/index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Suspense } from 'react'; // <-- Добавляем Suspense
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import './i18n'; // <-- Наш файл конфигурации
+import './i18n';
 import './index.css';
 import App from './App';
 
@@ -13,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* Suspense "ловит" момент загрузки переводов и показывает fallback */}
+    {}
     <Suspense fallback={<div>Loading...</div>}>
       <Provider store={store}>
         <BrowserRouter>
