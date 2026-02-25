8c6e8c05ae2b56f5a9f32532ab64e85ce3ad311a
Тимошенко Анна <timoshenkoas.pir231@gmail.com>
2026-02-26 01:42:56 +0300

    feat(server): Implement initial server startup and DB synchronization

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./db/index'); 
+const models = require('./db/models/models'); 
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Сервер работает!' });
+});
+
+const start = async () => {
+    try {
+
+        await sequelize.authenticate();
+        console.log('Подключение к БД было успешно установлено.');
+
+        await sequelize.sync(); 
+        console.log('Все модели были успешно синхронизированы.');
+
+        app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+
+    } catch (e) {
+        console.error('Не удалось подключиться к БД:', e);
+    }
+}
+
+start();
